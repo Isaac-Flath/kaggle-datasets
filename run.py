e8eebe3a02a399d7acaef53c2a66b042d18693e8
Isaac Flath <Isaac.flath@gmail.com>
2022-08-09 19:18:34 -0400

    code_to_run

---
+++ run.py
@@ -0,0 +1,12 @@
+from fastkaggle.core import *
+from pathlib import Path
+import os
+
+if __name__ == '__main__':
+    path = Path.home()/'.kaggle'
+    os.mkdir(path)
+    with open(path/'kaggle.json', 'w') as f: f.write(os.environ['KAGGLE_JSON'])
+
+    libs = open('libraries.txt').read().split('\n')
+    lib_path = Path('./kaggle_datasets')
+    create_libs_datasets(libs,lib_path,username)
