09c4fd7b244cc130253f4a28b5837f89186a244f
Isaac Flath <Isaac.flath@gmail.com>
2022-08-10 16:55:53 -0400

    updates

--- run.py
+++ run.py
@@ -2,6 +2,9 @@ from fastkaggle.core import *
 from fastcore.all import *
 from pathlib import Path
 import os
+import time
+
+
 
 if __name__ == '__main__':
     path = Path.home()/'.kaggle'
@@ -10,6 +13,17 @@ if __name__ == '__main__':
     with open(path/'kaggle.json', 'w') as f: f.write(os.environ['key'])
 
     libs = L(open('libraries.txt').read().split('\n')).filter(lambda x: x != '')
-    
     lib_path = Path('./kaggle_datasets')
-    create_libs_datasets(libs,lib_path,'isaacflath')
+    
+#    create_libs_datasets(libs,lib_path=lib_path,username='isaacflath',clear_after=False)
+
+    f = bind(create_libs_datasets,lib_path=lib_path,username='isaacflath',clear_after=False)
+    def tryf(lib):
+        try:
+            f(lib)
+        except:
+            print(f"API limit hit - sleep and retry....")
+            time.sleep(30)
+            f(lib)
+    
+    parallel(tryf,libs)
